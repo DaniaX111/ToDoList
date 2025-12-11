5fa9d86fe2f0aa7d970d74d90a6df4dc01686c89
Daniil Smaznov <daniilsma1105@gmail.com>
2025-12-11 07:21:41 +0500

    Second commit

--- obj/Debug/net9.0-maccatalyst/maccatalyst-arm64/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-arm64/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3609275ec8bc5f61fefe147c84efe04e7efeac1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
